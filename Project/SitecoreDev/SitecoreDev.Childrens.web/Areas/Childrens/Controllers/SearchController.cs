6d87cf59069deeb625213c9519a57ab4f80b809f
satyanand46 <satyanand46@gmail.com>
2021-06-16 17:24:38 +0530

    search

---
+++ Project/SitecoreDev/SitecoreDev.Childrens.web/Areas/Childrens/Controllers/SearchController.cs
@@ -0,0 +1,61 @@
+﻿using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Linq;
+using Sitecore.ContentSearch.Linq.Utilities;
+using SitecoreDev.Childrens.web.Areas.Childrens.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+
+namespace SitecoreDev.Childrens.web.Areas.Childrens.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Childrens/Search
+        public ActionResult Index()
+        {
+            var myResults = new SearchResults
+            {
+                Results = new List<SearchResult>()
+            };
+            string searchItem = Request.QueryString["searchItem"] ?? string.Empty;
+            var searchIndex = ContentSearchManager.GetIndex("sc10sxa_choa_web_index"); // Get the search index
+            var searchPredicate = GetSearchPredicate(searchItem); // Build the search predicate
+
+            using (var searchContext = searchIndex.CreateSearchContext()) // Get a context of the search index
+            {
+                var searchResults = searchContext.GetQueryable<SearchModel>().Where(searchPredicate); // Search the index for items which match the predicate
+
+                // This will get all of the results, which is not recommended
+                var fullResults = searchResults.GetResults();
+
+                foreach (var hit in fullResults)
+                {
+                    myResults.Results.Add(new SearchResult
+                    {
+                        Description = hit.Document.Description,
+                        Title = hit.Document.ItemName,
+                        fName = hit.Document.fName,
+                        fbody = hit.Document.fbody
+                    });
+                }
+                //return ResultJson(myResults);
+                return View("~/Areas/Childrens/Views/Search/Index.cshtml",myResults);
+            }
+        }
+
+        public static Expression<Func<SearchModel, bool>> GetSearchPredicate(string searchTerm)
+        {
+            var predicate = PredicateBuilder.True<SearchModel>();
+            // Search the whole phrase – CONTAINS
+          
+            predicate = predicate.Or(x => x.Description.Contains(searchTerm));
+            predicate = predicate.Or(x => x.Title.Contains(searchTerm));
+            predicate = predicate.Or(x => x.fName.Contains(searchTerm));
+            predicate = predicate.Or(x => x.fbody.Contains(searchTerm));
+
+            return predicate;
+        }
+    }
+}
---
+++ Project/SitecoreDev/SitecoreDev.Childrens.web/Areas/Childrens/Modules/SearchModel.cs
@@ -0,0 +1,38 @@
+﻿using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.SearchTypes;
+using System.Collections.Generic;
+
+
+namespace SitecoreDev.Childrens.web.Areas.Childrens.Modules
+{
+    public class SearchModel : SearchResultItem
+    {
+        [IndexField("_name")]
+        public virtual string ItemName { get; set; }
+
+        [IndexField("description_t")]
+        public virtual string Description { get; set; }
+        [IndexField("name_t")]
+        public virtual string Title { get; set; }
+        [IndexField("title_t")]
+        public virtual string fName { get; set; }
+        [IndexField("body_t")]
+        public virtual string fbody { get; set; }
+    }
+
+    public class SearchResult
+    {
+        public string Title { get; set; }      
+        public string Description { get; set; }
+        public string fName { get; set; }
+        public string fbody { get; set; }
+    }
+
+    /// <summary>
+    /// Custom search result model for binding to front end
+    /// </summary>
+    public class SearchResults
+    {
+        public List<SearchResult> Results { get; set; }
+    }
+}
